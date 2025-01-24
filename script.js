88572f9c16527493cc34c2e40710f47deb3091a2
mikirin123 <oya12yuyu@gmail.com>
2025-01-24 11:56:35 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -124,7 +124,9 @@ function resetFilters() {
 window.onscroll = function() {
     const scrollToTopBtn = document.getElementById('scrollToTopBtn');
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
+        if (window.innerWidth > 768) {
+            scrollToTopBtn.style.display = "block";
+        }
     } else {
         scrollToTopBtn.style.display = "none";
     }
