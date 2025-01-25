dd8529f7855464df220c4715ec8a6a4b95f3dfc0
mikirin123 <oya12yuyu@gmail.com>
2025-01-25 22:09:48 +0900

    Add files via upload

---
+++ details/script_d.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.banner_title').addEventListener('click', function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
