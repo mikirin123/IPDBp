16615a7c6358dcd5511a4b59236aeafd9eec5040
mikirin267 <12minecraft02@gmail.com>
2025-04-16 00:13:59 +0900

    調整

--- script.js
+++ script.js
@@ -178,8 +178,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 popupContent.innerHTML = '<div class="news-content"><b>メインページ</b><br><br>ゲーム内で実装されている全アイドルの詳細を確認できます。<br>フィルタ・並び替え・検索機能も活用してみてください。<br><br><hr><b>能力詳細</b><br><br>対象アイドルのステータスやスキル効果などを確認できます。<br><br><hr><b>ステータスランキング</b><br><br>全アイドルのステータスを一覧できます。<br>項目名をクリックすると、<br>昇順・降順で数値を並び替えることもできます。</div>';
                 popup.style.display = 'block';
                 popupOverlay.style.display = 'block';
-            } else if (menuName === 'このツールについて') {
-                popupContent.innerHTML = 'このツールはmikiが開発しています。<br>改善要望・不具合報告は<br><a href="https://x.com/miki_aipr">twitter</a>または<a href="https://forms.gle/Ke99JPag1pFawJw89">Googleフォーム</a>までお願いします。';
+            } else if (menuName === 'このサイトについて') {
+                popupContent.innerHTML = 'このサイトはmikiが作成しています。<br>改善要望・不具合報告は<br><a href="https://x.com/miki_aipr">twitter</a>または<a href="https://forms.gle/gM8HjG6Hzq4YxCmh9">Googleフォーム</a>までお願いします。';
                 popup.style.display = 'block';
                 popupOverlay.style.display = 'block';
             } 
